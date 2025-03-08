8c66597079e88133a2bfd11e67642565c948a823
viknesh1234 <vikneshr07@gmail.com>
2025-03-08 20:07:29 +0530

    code.java

---
+++ Main.java
@@ -0,0 +1,66 @@
+// Main.java
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummyHead.next;
+    }
+}
+
+public class Main {
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Creating first linked list: 2 -> 4 -> 3
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+
+        // Creating second linked list: 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        // Solving the problem
+        Solution solution = new Solution();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        // Printing the result
+        System.out.print("Result: ");
+        printList(result);
+    }
+}

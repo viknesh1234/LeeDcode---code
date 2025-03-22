906f5371e46a42c2424650306bef773e2cbe6a99
viknesh1234 <vikneshr07@gmail.com>
2025-03-22 20:36:42 +0530

    code Java

---
+++ CountingBits.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class CountingBits {
+    public static int[] countBits(int n) {
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            f[i] = Integer.bitCount(i);
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;  // Change this value to test different cases
+        int[] result = countBits(n);
+        
+        System.out.println("Input: n = " + n);
+        System.out.println("Output: " + Arrays.toString(result));
+    }
+}

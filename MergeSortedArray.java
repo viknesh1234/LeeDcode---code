6354feb7fb399677e57d1baac2d03b9a49ff977a
viknesh1234 <vikneshr07@gmail.com>
2025-04-02 22:08:14 +0530

    new code

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -20,8 +20,7 @@ public class MergeSortedArray {
             nums1[k--] = nums2[j--];
         }
     }
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};  
         int m = 3; 
         int[] nums2 = {2, 5, 6}; 

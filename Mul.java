8f075b631c5eb9f69454152687309a2fd0f20759
viknesh1234 <vikneshr07@gmail.com>
2025-07-04 19:56:35 +0530

    new one

---
+++ Mul.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Mul {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+         int n1=3;
+         int n2=4;
+            System.out.println("n1^n2");
+        
+        
+
+    }
+}
---
+++ for.java
@@ -0,0 +1,9 @@
+public class For {
+    public static void main(String[]args){
+        int i=0;
+        for(i=1;i<=3;i++){
+            System.out.println("i");
+        }
+    }
+
+}
